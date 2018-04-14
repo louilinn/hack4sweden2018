ade8d6c5921bd4220575b4f887868e5f607feaec
xeduses <c17fpi@cs.umu.se>
2018-04-14 15:38:01 +0200

    Cleanup and 404 case.

--- socStyrelsenRequest/__main__.py
+++ socStyrelsenRequest/__main__.py
@@ -5,40 +5,21 @@ from socRequest import SocRequest
 class GetHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        parsed_path = parse.urlparse(self.path)
-        message_parts = [
-            'CLIENT VALUES:',
-            'client_address={} ({})'.format(
-                self.client_address,
-                self.address_string()),
-            'command={}'.format(self.command),
-            'path={}'.format(self.path),
-            'real path={}'.format(parsed_path.path),
-            'query={}'.format(parsed_path.query),
-            'request_version={}'.format(self.request_version),
-            '',
-            'SERVER VALUES:',
-            'server_version={}'.format(self.server_version),
-            'sys_version={}'.format(self.sys_version),
-            'protocol_version={}'.format(self.protocol_version),
-            '',
-            'HEADERS RECEIVED:',
-        ]
-        for name, value in sorted(self.headers.items()):
-            message_parts.append(
-                '{}={}'.format(name, value.rstrip())
-            )
-        message_parts.append('')
-        message = '\r\n'.join(message_parts)
-        self.send_response(200)
 
-        self.send_header('Content-Type',
+        print(self.path)
+        if(self.path == '/soc2026'):
+            self.send_response(200)
+            self.send_header('Content-Type',
                          'text/plain; charset=utf-8')
-        self.end_headers()
+            self.end_headers()
+            self.wfile.write(SocRequest().getSuicideJson().encode('utf-8'))
 
-        # TODO: Send response depending on what is requestedin header
-        self.wfile.write(SocRequest().getSuicideJson().encode('utf-8'))
-        #self.wfile.write(message.encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type',
+                         'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Invalid API Path".encode('utf-8'))
 
 if __name__ == '__main__':
     from http.server import HTTPServer
