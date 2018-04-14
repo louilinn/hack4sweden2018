5379518e82b8eba1ad57fed6cd976b98a2975206
Louise Linné <linne.louise@gmail.com>
2018-04-14 21:03:00 +0200

    swedish characters handled

--- __main__.py
+++ __main__.py
@@ -29,12 +29,12 @@ class GetHandler(BaseHTTPRequestHandler):
         elif(self.path.startswith('/')):
             if self.path == '/':
                 self.path += 'index.html'
-                
+
             self.send_response(200)
             self.send_header('Content-Type',
                          'text/html; charset=utf-8')
             self.end_headers()
-            self.wfile.write(open("./app" + self.path).read().encode('utf-8'))
+            self.wfile.write(open("./app" + self.path).read().encode('ISO-8859-1'))
 
 
 
